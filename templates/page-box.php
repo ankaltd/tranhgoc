1d9c3aad12ca83d65e260b74716460fb3651a828
duongcgc <duongcgc@gmail.com>
2023-08-16 00:52:22 +0700

    u

--- templates/page-box.php
+++ templates/page-box.php
@@ -1,18 +1,22 @@
 <?php
 
 /**
- * Template Name: WEP  Builder
+ * Template Name: Box Container
  */
 
 // Header
 get_header();
 
+echo '<div class="container">';
+
 /* Start the Loop */
 while (have_posts()) :
     the_post();
-    the_content();    
-endwhile; 
+    the_content();
+endwhile;
 // End of the loop.
+
+echo '</div>';
 
 /* Footer */
 get_footer();
--- templates/page-full.php
+++ templates/page-full.php
@@ -1,36 +1,18 @@
 <?php
 
-
-
 /**
-
- * Template Name: Giới thiệu 
-
+ * Template Name: Full Width
  */
 
-
-
 // Header
-
 get_header();
 
-
-
 /* Start the Loop */
-
 while (have_posts()) :
-
     the_post();
-
-    the_content();
-
-endwhile;
-
+    the_content();    
+endwhile; 
 // End of the loop.
 
-
-
 /* Footer */
-
 get_footer();
-
