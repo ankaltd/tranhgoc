b5468540bb97839d55f8c24c0ea1fdac8a6ff702
duongcgc <duongcgc@gmail.com>
2023-08-26 17:57:16 +0700

    images for template

--- app/config/style-files.config.php
+++ app/config/style-files.config.php
@@ -40,7 +40,7 @@ return apply_filters('wep_css_files', [
 
         'handle'        => 'wep-font',
 
-        'path'          => 'https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&display=swap',
+        'path'          => 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&display=swap',
 
         'dependencies'  => [],
 
